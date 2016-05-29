d0c53df5770a3ec1c32a1d3816aaae9396abb27c
Bruce James <bruce.james@clear.net.nz>
2016-05-29 16:49:49 +1200

    Added nav-to-state directive from tripReports (so this can be embedded in the Joomla website)

---
+++ app/common/nav-to-state-directive.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    'use strict';
+
+    // Directive to navigate to new state 
+    angular.module('tripApp').directive('navToState', [function () {
+
+        var controller = ['$scope', '$state',
+            function ($scope, $state) {
+                // get the url for the specified state via the $state service
+                var paramsObj = $scope.$eval(this.params); // convert string to object
+                var url = $state.href(this.name, paramsObj);
+                url = url.replace('#/', ''); // trim leading part of url (I guess this is added again elsewhere?)
+                this.url = encodeURIComponent(url);
+
+                this.navigateToState = function (event) {
+                    // This is the key -> preventing default navigation
+                    event.preventDefault();
+
+                    $state.go(this.name, paramsObj);
+                };
+            }];
+
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            scope: {},
+            bindToController: {
+                name: '@',
+                params: '@'
+            },
+            controller: controller,
+            controllerAs: 'navToStateController',
+            template:
+                '<a ' +
+                    'ng-href="../index.php/trip?goto={{navToStateController.url}}" ' +
+                    'ng-click="navToStateController.navigateToState($event)" ' +
+                    'ng-transclude>' +
+                '</a>'
+        };
+    }]);
+
+}());
