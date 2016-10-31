4342ce61eb0afc590a1c54dd56b3d0fa79e0bf29
Richard Lobb <richard.lobb@canterbury.ac.nz>
2016-10-31 15:57:46 +1300

    Filter POST data before display.

--- printabletriplist.php
+++ printabletriplist.php
@@ -8,12 +8,32 @@
 <body>
     <!--! ?php var_dump($_POST)?-->
     <h1>Christchurch Tramping Club Trip List</h1>
-    <h2><?php echo $_POST['title']?></h2>
+    <h2><?php
+    function input($key, $index=null) {
+        // Return the sanitised POST variable with the given key.
+        // If the value is an array and a specific index is required, set
+        // $index to an int index value. If the whole array is required, set it
+        // to the string value 'ALL'.
+        if ($index === NULL) {
+            return filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+        } else {
+            $data = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS, FILTER_REQUIRE_ARRAY);
+            if ($index === 'ALL') {
+                return $data;
+            } else {
+                return $data[$index];
+            }
+        }
+    }
+
+    echo input('title');
+    ?></h2>
+
     <table class="triplistparams">
         <tr>
-            <td class="paramlabel">Leader:</td><td class="leadername"><?php echo $_POST['leadername']?>
-            <td class="paramlabel">Date:</td><td class="date"><?php echo $_POST['date']?></td>
-            <td class="paramlabel">Length:</td><td class="length"><?php echo $_POST['length']?> days</td>
+            <td class="paramlabel">Leader:</td><td class="leadername"><?php echo input('leadername')?>
+            <td class="paramlabel">Date:</td><td class="date"><?php echo input('date')?></td>
+            <td class="paramlabel">Length:</td><td class="length"><?php echo input('length')?></td>
         </tr>
     </table>
     <h3>Participants</h3>
@@ -26,12 +46,12 @@
         </tr>
         <?php
             $i = 0;
-            foreach ($_POST['name'] as $name) { ?>
+            foreach (input('name', 'ALL') as $name) { ?>
         <tr>
             <td><?php echo $name?></td>
-            <td><?php echo $_POST["email"][$i]?></td>
-            <td><?php echo $_POST["phone"][$i]?></td>
-            <td><?php echo $_POST["hascar"][$i]?></td>
+            <td><?php echo input("email", $i)?></td>
+            <td><?php echo input("phone", $i)?></td>
+            <td><?php echo input("hascar", $i)?></td>
         </tr>
             <?php
             $i += 1;
@@ -44,7 +64,7 @@
     </table>
     <h3>Notes</h3>
     <?php if (!empty($_POST['note'])) {
-        foreach ($_POST['note'] as $note) {
+        foreach (input('note', 'ALL') as $note) {
             echo $note;
             echo "\n<br>";
         }
