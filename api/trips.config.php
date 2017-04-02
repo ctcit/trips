7bfa108602124eeb564f2571235e17d3a64d7490
Richard Lobb <richard.lobb@canterbury.ac.nz>
2017-04-02 21:02:33 +1200

    Update repository to match live site config.

--- api/trips.config.php
+++ api/trips.config.php
@@ -3,8 +3,8 @@
 class TripConfig
 {
 	const EditorRoles = "'Webmaster','Overnight Trip Organiser','Day Trip Organiser','Club Captain'";
-	const EmailFilter = "/^((alastairgbrown@yahoo\.com\.au)|(bruce\.s\.james@gmail\.com))$/";
-	const BaseUrl = "http://www.ctc.org.nz/index.php/current-trips-dev";
+	const EmailFilter = "/^.*$/";
+	const BaseUrl = "http://www.ctc.org.nz/index.php/current-trips";
 	const EmailImageUrl = "http://www.ctc.org.nz/tripsignup.dev/api/EmailImage.php";
 	const ShowDebugUpdate = false;
 	const AdditionalLines = 5;
